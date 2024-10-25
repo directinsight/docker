e21b459f276a5d60676d1de173ad04a1b60ab4b9
William White <william.white@directinsight.co.uk>
2024-10-25 11:16:39 +0100

    modified:   build.sh

--- yocto-hardknott_karo-bsp/build.sh
+++ yocto-hardknott_karo-bsp/build.sh
@@ -1,13 +1,5 @@
-#docker build -t yocto:hardknott_test-v1 .
 docker build -t wrwdi/yocto-hardknott_karo-bsp:latest .
 
-# docker run --name yocto-hardknott_karo-bsp -it --rm --workdir /home/yoctosdk -v /home/armsdk/docker/volume/yocto:/home/yoctosdk wrwdi/yocto-hardknott_karo-bsp:latest
-
-
-## Tests
-## -----
-
-## run sudo connands eg "sudo su"
-## Exit container removes it
-## Path setup correctly even after second attach
+## Once the container is build, run as follows
+# docker run --name yocto-hardknott_karo-bsp -it --rm -v /home/armsdk/docker/volume/yocto:/home/yoctosdk wrwdi/yocto-hardknott_karo-bsp:latest
 
