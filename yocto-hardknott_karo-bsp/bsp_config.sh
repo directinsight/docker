b4e7d995a1603206fab44573d1515f6c91920554
William White <william.white@directinsight.co.uk>
2024-10-25 10:45:41 +0100

    new file:   yocto-hardknott_karo-bsp/Dockerfile 	new file:   yocto-hardknott_karo-bsp/bsp_config.sh 	new file:   yocto-hardknott_karo-bsp/build.sh

---
+++ yocto-hardknott_karo-bsp/bsp_config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git config --global user.name "yoctosdk"
+git config --global user.email "yoctosdk@yoctosdk.com" 
+git config --global color.ui auto
+mkdir /home/yoctosdk/bin
+curl https://storage.googleapis.com/git-repo-downloads/repo > /home/yoctosdk/bin/repo
+chmod a+x /home/yoctosdk/bin/repo
+## The PATH ise set in /etc/profile
+#echo "export PATH=~/bin:$PATH" >> /home/yoctosdk/.bashrc
+#export PATH=~/bin:$PATH
+mkdir /home/yoctosdk/karo-bsp
+cd /home/yoctosdk/karo-bsp
+repo init -u https://github.com/karo-electronics/karo-bsp -b hardknott
+repo sync
