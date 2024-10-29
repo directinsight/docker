d00f38672dfcdf9acf28d97dcb7d97a4f3e6317c
William White <william.white@directinsight.co.uk>
2024-10-29 16:36:15 +0000

    modified:   yocto-mickledore_karo-nxp-bsp/build.sh

--- yocto-mickledore_karo-nxp-bsp/build.sh
+++ yocto-mickledore_karo-nxp-bsp/build.sh
@@ -8,18 +8,18 @@ docker build -t wrwdi/yocto-mickledore_karo-nxp-bsp:latest .
 # mkdir -p $HOME/$TARGET_BUILD_DIR  
 
 ## Start the container and mount the build directory on the host:
-# docker run --name yocto-hardknott_karo-bsp --rm -it -v $HOME/$TARGET_BUILD_DIR:/home/yoctosdk wrwdi/yocto-hardknott_karo-bsp
+# docker run --name yocto-mickledore_karo-bsp --rm -it -v $HOME/$TARGET_BUILD_DIR:/home/yoctosdk wrwdi/yocto-mickledore_karo-bsp
 
 ## In the container, configure the BSP:
 # source /bsp_config.sh
 # cd karo_bsp  
 # repo sync 
-# DISTRO=karo-minimal MACHINE=txul-5011 source setup-environment build-5011-minimal 
+# DISTRO=karo-minimal MACHINE=qs93-5210 source setup-environment build-qs93-5210
 
 ## Note that you may need to run the above command twice.  
 
 ## Enable sstate cache:
-# echo SSTATE_MIRRORS = \"file://.* http://sstate.karo-electronics.de/hardknott/PATH\" >> conf/local.conf
+# echo SSTATE_MIRRORS = \"file://.* http://sstate.karo-electronics.de/mickledore/PATH\" >> conf/local.conf 
 
 ## Build an image:     
 # bitbake karo-image-minimal
